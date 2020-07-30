b4ed4c7945c39ead83e7c9369a0120f4d36d8ffb
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-07-30 19:51:40 -0400

    styling and some minor error handling

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -35,7 +35,10 @@ exports.postTask = (req, res) => {
 
     if (moment(startTime).isAfter(endTime)) {
         return res.render('schedule', {
-        error: "Task end time should be after start time",
+        error: {
+            err: 'time-issye',
+            msg: "Task end time should be after start time"
+        },
         weeklySchedule: []
         })
     }
@@ -157,12 +160,20 @@ exports.viewSchedule = (req, res) => {
     const {driver, week} = req.body
     let weeklySchedule;
     scheduleData[driver][week] ? weeklySchedule = scheduleData[driver][week] : weeklySchedule = [];
-    
-    res.render('schedule', {
-        error: null,
-        weeklySchedule
-    })
-}
+    if (weeklySchedule.length) {
+        res.render('schedule', {
+            error: null,
+            weeklySchedule
+        })
+    } else {
+        res.render('schedule', {
+            error: {
+                err: 'driver-no-tasks',
+                msg: "This driver currently has no tasks for the selected week"
+            }
+        }) 
+    }
+    }
 
 exports.editTask = (req, res) => {
     if (!scheduleData) scheduleData = createScheduleData();
