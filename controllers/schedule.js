175306cef83295a18600274cedd1c2fd98ab0662
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-07-30 21:04:10 -0400

    should download file to users computer

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const Moment = require('moment');
 const MomentRange = require('moment-range');
@@ -308,7 +309,7 @@ exports.downloadSchedule = (req, res) => {
     const driverSchedule = scheduleData[driver];
 
     handleInterval(driverSchedule, scheduleInterval)
-    res.status(200).render('schedule', {
+    res.status(200).download(path.join(__dirname, 'schedule.csv').render('schedule', {
         error: null,
         weeklySchedule: driverSchedule['1']
     })
