d32bf22c7a0d0ad63c19ab7465ee8e4eae002938
Kevin Guertin <32689269+kguertin@users.noreply.github.com>
2020-07-30 20:50:52 -0400

    update to handle heroku deploy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,11 +19,3 @@ app.use(scheduleRoutes);
 app.listen(PORT, () => {
     console.log(`Server connected on port ${PORT}`);
 }) 
-
-// style
-
-// if we have time:
-// make csv file download in browser rather than write to drive
-//rework how we store data so we can use filter to handle everything
-// deploy
-// ui/ux
